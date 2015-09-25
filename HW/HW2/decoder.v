18a3d8634ef66c25689b8f10aa2e9004a2ad4e05
Ryan Eggert <Ryan.Eggert@students.olin.edu>
2015-09-25 00:43:38 -0400

    Combined Testbench
    Combined decoder testbench (i.e. test both structural and behavioral in
    one module, one table).

--- HW/HW2/decoder.v
+++ HW/HW2/decoder.v
@@ -29,29 +29,29 @@ endmodule
 module testDecoder; 
 reg addr0, addr1;
 reg enable;
-wire out0,out1,out2,out3;
-// behavioralDecoder decoder (out0, out1, out2, out3, addr0, addr1, enable);
-structuralDecoder decoder (out0,out1,out2,out3,addr0,addr1,enable); // Swap after testing
+wire beh_out0, beh_out1, beh_out2, beh_out3;
+wire str_out0, str_out1, str_out2, str_out3;
+behavioralDecoder beh_decoder (beh_out0, beh_out1, beh_out2, beh_out3, addr0, addr1, enable);
+structuralDecoder str_decoder (str_out0, str_out1, str_out2, str_out3, addr0, addr1, enable); // Swap after testing
 
 initial begin
-$display("En A0 A1| O0 O1 O2 O3 | Expected Output");
+$display(" Inputs | Behavioral  | Structural  | Expected");
+$display("En A0 A1| O0 O1 O2 O3 | O0 O1 O2 O3 | Outputs ");
 enable=0;addr0=0;addr1=0; #1000 
-$display("%b  %b  %b |  %b  %b  %b  %b | All false", enable, addr0, addr1, out0, out1, out2, out3);
+$display("%b  %b  %b |  %b  %b  %b  %b |  %b  %b  %b  %b | All false", enable, addr0, addr1, beh_out0, beh_out1, beh_out2, beh_out3,  str_out0, str_out1, str_out2, str_out3);
 enable=0;addr0=1;addr1=0; #1000
-$display("%b  %b  %b |  %b  %b  %b  %b | All false", enable, addr0, addr1, out0, out1, out2, out3);
+$display("%b  %b  %b |  %b  %b  %b  %b |  %b  %b  %b  %b | All false", enable, addr0, addr1, beh_out0, beh_out1, beh_out2, beh_out3,  str_out0, str_out1, str_out2, str_out3);
 enable=0;addr0=0;addr1=1; #1000 
-$display("%b  %b  %b |  %b  %b  %b  %b | All false", enable, addr0, addr1, out0, out1, out2, out3);
+$display("%b  %b  %b |  %b  %b  %b  %b |  %b  %b  %b  %b | All false", enable, addr0, addr1, beh_out0, beh_out1, beh_out2, beh_out3,  str_out0, str_out1, str_out2, str_out3);
 enable=0;addr0=1;addr1=1; #1000 
-$display("%b  %b  %b |  %b  %b  %b  %b | All false", enable, addr0, addr1, out0, out1, out2, out3);
+$display("%b  %b  %b |  %b  %b  %b  %b |  %b  %b  %b  %b | All false", enable, addr0, addr1, beh_out0, beh_out1, beh_out2, beh_out3,  str_out0, str_out1, str_out2, str_out3);
 enable=1;addr0=0;addr1=0; #1000 
-$display("%b  %b  %b |  %b  %b  %b  %b | O0 Only", enable, addr0, addr1, out0, out1, out2, out3);
+$display("%b  %b  %b |  %b  %b  %b  %b |  %b  %b  %b  %b | O0 Only", enable, addr0, addr1, beh_out0, beh_out1, beh_out2, beh_out3,  str_out0, str_out1, str_out2, str_out3);
 enable=1;addr0=1;addr1=0; #1000 
-$display("%b  %b  %b |  %b  %b  %b  %b | O1 Only", enable, addr0, addr1, out0, out1, out2, out3);
+$display("%b  %b  %b |  %b  %b  %b  %b |  %b  %b  %b  %b | O1 Only", enable, addr0, addr1, beh_out0, beh_out1, beh_out2, beh_out3,  str_out0, str_out1, str_out2, str_out3);
 enable=1;addr0=0;addr1=1; #1000 
-$display("%b  %b  %b |  %b  %b  %b  %b | O2 Only", enable, addr0, addr1, out0, out1, out2, out3);
+$display("%b  %b  %b |  %b  %b  %b  %b |  %b  %b  %b  %b | O2 Only", enable, addr0, addr1, beh_out0, beh_out1, beh_out2, beh_out3,  str_out0, str_out1, str_out2, str_out3);
 enable=1;addr0=1;addr1=1; #1000 
-$display("%b  %b  %b |  %b  %b  %b  %b | O3 Only", enable, addr0, addr1, out0, out1, out2, out3);
+$display("%b  %b  %b |  %b  %b  %b  %b |  %b  %b  %b  %b | O3 Only", enable, addr0, addr1, beh_out0, beh_out1, beh_out2, beh_out3,  str_out0, str_out1, str_out2, str_out3);
 end
 endmodule
-
-
